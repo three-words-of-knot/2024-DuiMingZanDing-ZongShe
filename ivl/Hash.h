4cab602098f9062024dae5ab1b356a1438b6a0cb
希儿单推人 <hh75624777163@163.com>
2024-10-21 16:39:39 +0800

    哈希表框架初建
    还没想好怎么写

---
+++ ivl/Hash.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+#ifndef HASH_H
+#define HASH_H
+
+class HashMap {
+public:
+	vector<int> nodeMap;
+
+
+private:
+	int _FindInNodeMap(int key1,int key2) {
+
+	}
+};
+#endif 
